e10cdc71eb1a110196b11f0f51ed6f17e24630f2
neche <chi2064real@gmail.com>
2022-04-04 10:02:54 +0100

    List for beginner code

---
+++ bin/list.dart
@@ -0,0 +1,29 @@
+void main(List<String> arguments) {
+
+//  List names =[
+//   "Chinecherem",
+//   "Amoge",
+//   "Harry",
+//   "Sussan",
+//   "Simon"
+//  ];
+//  print(names[4]);
+//  names.add("Ife");
+//  names.remove("Simon");
+//  print(names);
+var add = false;
+ var names = [
+   "Chi",
+   "Tina",
+   for(var i =1; i < 6; i++)"Ugwu $i",
+//   "Ugwu",
+ ];
+ /**var names1 = [
+   "Anyi",
+   ...names
+ ];
+**/
+ for(var name in names){
+   print(name);
+ }
+}
